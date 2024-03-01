91c1daa8b070e554c1340adf8c215f0e21c3ca7d
Joey <joey.mckenzie27@gmail.com>
2024-03-01 10:08:23 -0800

    chore: remove unnecessary dirs for rector

--- rector.php
+++ rector.php
@@ -10,10 +10,6 @@ use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRect
 return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/app',
-        __DIR__.'/bootstrap',
-        __DIR__.'/public',
-        __DIR__.'/resources',
-        __DIR__.'/routes',
         __DIR__.'/tests',
     ])
     ->withPhpSets(php83: true)
