445147883534c6d9203f2b459e2ceb4ec2f71935
Joey <joey.mckenzie27@gmail.com>
2024-03-01 10:32:25 -0800

    chore: cleanup

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function (): void {
-    $this->comment(Inspiring::quote());
+    $this->comment(Inspiring::quote()); // @phpstan-ignore-line
 })->purpose('Display an inspiring quote')->hourly();
