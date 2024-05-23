c64e31078910b1b47a285ad37d21350d218ebaaf
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-05-22 22:10:55 -0700

    chore: wip

--- resources/js/components/NavLink.tsx
+++ resources/js/components/NavLink.tsx
@@ -13,8 +13,8 @@ export default function NavLink({
             className={clsx(
                 "inline-flex items-center border-b-2 px-1 pt-1 font-medium text-sm leading-5 transition duration-150 ease-in-out focus:outline-none",
                 active
-                    ? "border-primary text-gray-900 focus:border-primary dark:text-neutral-200"
-                    : "border-transparent text-gray-500 focus:border-gray-300 hover:border-gray-300 focus:text-gray-700 hover:text-gray-700",
+                    ? "border-primary focus:border-primary"
+                    : "border-transparent text-neutral-500 focus:border-gray-300 hover:border-gray-300 focus:text-gray-700 hover:text-gray-700",
                 className,
             )}
         >
