bc200cb163558a094cc428c79c3039218492fd73
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-05-22 21:34:54 -0700

    chore: wip

--- resources/js/components/NavLink.tsx
+++ resources/js/components/NavLink.tsx
@@ -1,4 +1,5 @@
 import { type InertiaLinkProps, Link } from "@inertiajs/react";
+import { clsx } from "clsx";
 
 export default function NavLink({
     active = false,
@@ -9,11 +10,13 @@ export default function NavLink({
     return (
         <Link
             {...props}
-            className={`inline-flex items-center border-b-2 px-1 pt-1 font-medium text-sm leading-5 transition duration-150 ease-in-out focus:outline-none${
+            className={clsx(
+                "inline-flex items-center border-b-2 px-1 pt-1 font-medium text-sm leading-5 transition duration-150 ease-in-out focus:outline-none",
                 active
                     ? "border-primary text-gray-900 focus:border-primary dark:text-neutral-200"
-                    : "border-transparent text-gray-500 focus:border-gray-300 hover:border-gray-300 focus:text-gray-700 hover:text-gray-700"
-            }${className}`}
+                    : "border-transparent text-gray-500 focus:border-gray-300 hover:border-gray-300 focus:text-gray-700 hover:text-gray-700",
+                className,
+            )}
         >
             {children}
         </Link>
